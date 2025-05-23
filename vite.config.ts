9a2530a13faca49357177120d75ebcdc11b98afe
paris <paris@macbookpro.mynetworksettings.com>
2025-05-23 11:41:34 -0400

    explicitly allow serving vite on all domains

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true,
+    allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', '.kinsta.app'],
   },
 })
